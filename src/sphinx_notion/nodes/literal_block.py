00186d540e087e375531ebfa14c9023e401ab711
ftnext <takuyafjp+develop@gmail.com>
2025-08-19 00:18:31 +0900

    [refactor] Simplify language handling

--- src/sphinx_notion/nodes/literal_block.py
+++ src/sphinx_notion/nodes/literal_block.py
@@ -1,14 +1,14 @@
-def to_notion_language(pygments_language: str) -> str:
-    if pygments_language == "default":
-        # default means "not specified"
+PygmentsLanguage = str
+
+_as_python: set[PygmentsLanguage] = {"python", "pycon"}
+# default means "not specified"
+_as_plain_text: set[PygmentsLanguage] = {"default", "pytb", "text", "output"}
+
+
+def to_notion_language(pygments_language: PygmentsLanguage) -> str:
+    if pygments_language in _as_plain_text:
         return "plain text"
-    if pygments_language == "text":
-        return "plain text"
-    if pygments_language == "pytb":
-        return "plain text"
-    if pygments_language == "output":
-        return "plain text"
-    if pygments_language == "pycon":
+    if pygments_language in _as_python:
         return "python"
     # TODO: Support for other languages
     return pygments_language
