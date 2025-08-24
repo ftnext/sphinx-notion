350abb78c7380ce2b76c6909e853fcfcca84ee1b
ftnext <takuyafjp+develop@gmail.com>
2025-08-24 23:41:40 +0900

    [feat] Add confval sphinx_notion_character_upper_limit

--- src/sphinx_notion/__init__.py
+++ src/sphinx_notion/__init__.py
@@ -4,6 +4,13 @@ from sphinx_notion.builders import NotionBuilder
 
 __version__ = "0.0.4"
 
+NOTION_API_CHARACTER_UPPER_LIMIT = 2_000
+
 
 def setup(app: Sphinx):
+    app.add_config_value(
+        "sphinx_notion_character_upper_limit",
+        NOTION_API_CHARACTER_UPPER_LIMIT,
+        True,
+    )
     app.add_builder(NotionBuilder)
